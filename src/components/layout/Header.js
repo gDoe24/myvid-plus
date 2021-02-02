cf7e497fda50331b2c7b3c6251c25b092167cceb
gDoe24 <gregkeel11@gmail.com>
2021-02-01 19:00:58 -0500

    feat: Header CSS and NavBar for Header/Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,36 +4,47 @@ function Header(){
     return(
         <Fragment>
         <header>
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
-            <div class="container-fluid">
-            <a class="navbar-brand" href="#">Expand at md</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-md fixed-top">
+            <div className="container-fluid">
+            <a className="navbar-brand" href="#">MyVids Plus</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarsExample04">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item active">
-                    <a class="nav-link" aria-current="page" href="#">Home</a>
+            <div className="collapse navbar-collapse" id="navbarsExample04">
+                <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item active">
+                    <a className="nav-link disabled" aria-current="page" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Movies</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#" tabIndex="-1" aria-disabled="true">TV Shows</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdown04">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false">Upcoming</a>
+                    <ul className="dropdown-menu" aria-labelledby="dropdown04">
+                    <li><a className="dropdown-item" href="#">Movies</a></li>
+                    <li><a className="dropdown-item" href="#">TV Shows</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
                     </ul>
                 </li>
-                </ul>
-                <form>
-                    <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+                <span className="navbar-nav mx-5">
+                <i className="bi bi-search mx-2 pt-1"></i>
+                <form className="home-search-form ">
+                    <input className="home-search form-control " type="text" placeholder="Search" />
                 </form>
+                </span>
+                </ul>
+                <ul className="navbar-nav mx-5">
+                    <li className="nav-item">
+                        <a className="login nav-link" href="#">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="login nav-link" href="#">Register</a>
+                    </li>
+                </ul>
             </div>
             </div>
             </nav>
