5d0fdf89b77b44c7da2b122a039fe62af1f0dca1
gDoe24 <gregkeel11@gmail.com>
2021-02-03 15:52:07 -0500

    refactor: featured section Added glass box and picture to featured section

--- src/components/movies/featured.js
+++ src/components/movies/featured.js
@@ -4,9 +4,9 @@ function Featured(){
     return (
         <Fragment>
         <section className="home-featured">
-                <div className="featured-pic mx-4">
-                </div>
-                <div className="featured-info mt-5 mx-4">
+                <img className="featured-pic mx-3" src={process.env.PUBLIC_URL + '/Tenent.jpg'}>
+                </img>
+                <div className="featured-info mx-2">
                     <div className="fi-title-score">
                         <h1 className="fw-dark" id="fi-title">Title</h1>
                         <div className="fi-score"></div>
@@ -16,7 +16,7 @@ function Featured(){
                     <a href="#" className="btn btn-primary my-2 me-3">Play</a>
                     <a href="#" className="btn btn-secondary my-2 ms-3">Watch Trailer</a>
                     </div>
-                    <h3 className="mt-3 mb-1">Overview</h3>
+                    <h3 className="mt-1 mb-1">Overview</h3>
                     <p className="lead text-muted " id="fi-sum">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                     ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
@@ -26,7 +26,7 @@ function Featured(){
                     <p>
                     </p>
                 </div>
-                <div className="featured-cd mt-5 mx-4">
+                <div className="featured-cd mx-4">
                     <h1 className="fw-dark">Director/Cast</h1>
                 </div>
         </section>
