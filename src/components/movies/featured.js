84db3dfd8bff177a51b38c6dff4eb2251518de71
gDoe24 <gregkeel11@gmail.com>
2021-02-08 10:06:03 -0500

    refactor: featured CSS Modified the CSS for the featured section

--- src/components/movies/featured.js
+++ src/components/movies/featured.js
@@ -4,12 +4,12 @@ import React, { Fragment } from 'react';
 const array = ["Lebron James", "Kobe Bryant", "Michael Jordan", "John Cena", "Deshaun Watson"];
 
 const featuredMovies = [
-                      {title: "Tenent",
+                      {title: "Tenet",
                        overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \
                        voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                       image: '/Tenent.jpg',
+                       image: 'https://www.themoviedb.org/t/p/original//k68nPLbIST6NP96JmTxmZijEvCA.jpg',
                        cast: array
                         },
                         {title: "Your Name.",
@@ -17,7 +17,7 @@ const featuredMovies = [
                         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
                         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \
                         voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                        image: '/Tenent.jpg',
+                        image: 'https://www.themoviedb.org/t/p/original/a954X8xMnukqMracTipsK3T5lD3.jpg',
                         cast: array
                          }
                       ];
@@ -38,7 +38,7 @@ function Featured(){
               return(
                 <div className={(idx == 0? "carousel-item active": "carousel-item")}>
                   <section className="home-featured">
-                    <img className="featured-pic mx-3" src={process.env.PUBLIC_URL + movie.image}>
+                    <img className="featured-pic mx-3" src={movie.image}>
                     </img>
                     <div className="featured-info mx-2">
                       <div className="fi-title-score">
@@ -50,13 +50,13 @@ function Featured(){
                       <a href="#" className="btn fi-play my-2 me-3"><i className="bi bi-play-fill"></i>Play</a>
                       <a href="#" className="btn fi-trailer my-2 ms-3"><i className="bi bi-film"></i>Watch Trailer</a>
                       </div>
-                      <h3 className="mt-1 mb-3">Overview</h3>
-                      <p className="lead " id="fi-sum">{movie.overview}</p>
+                      <h3 className="fi-overview mt-1 mb-3">Overview</h3>
+                      <p className="fi-overview-p lead " id="fi-sum">{movie.overview}</p>
                   </div>
                   <div className="featured-cd mx-4">
                       <h2 className="fw-dark mb-4">Director/Cast</h2>
                       {movie.cast.map((name) =>{
-                          return(<p key={name}> {name} </p>)
+                          return(<p className="featured-cd-p"key={name}> {name} </p>)
                       })} 
                   </div>
                   </section>
