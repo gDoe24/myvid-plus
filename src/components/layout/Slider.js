e5345b3b320a4f6c80f09e2b1fbc3e9ed2bb5fdf
gDoe24 <gregkeel11@gmail.com>
2021-02-08 15:33:45 -0500

    feat: Slider.js styled the Slider cards and images

--- src/components/layout/Slider.js
+++ src/components/layout/Slider.js
@@ -1,68 +1,58 @@
 import React, { Fragment } from 'react';
 
+const featuredMovies = [
+    {
+     title: "Tenet",
+     image: 'https://www.themoviedb.org/t/p/original//k68nPLbIST6NP96JmTxmZijEvCA.jpg'
+    },
+    {
+     title: "Your Name.",
+     image: 'https://www.themoviedb.org/t/p/original/a954X8xMnukqMracTipsK3T5lD3.jpg'
+    },
+    {
+     title: "Demon Slayer: Infinity Train",
+     image: 'https://www.themoviedb.org/t/p/original//yF45egpHwaYLn4jTyZAgk0Cmug9.jpg'
+    },
+    { 
+     title: "Soul",
+     image: 'https://www.themoviedb.org/t/p/original/hm58Jw4Lw8OIeECIq5qyPYhAeRJ.jpg'
+    },
+    {
+     title: "The Dark Knight Rises",
+     image: 'https://www.themoviedb.org/t/p/original//vzvKcPQ4o7TjWeGIn0aGC9FeVNu.jpg'
+    },
+    {
+        title: "Avengers: Endgame",
+        image: 'https://www.themoviedb.org/t/p/original/ulzhLuWrPK07P1YkdWQLZnQh1JL.jpg'
+       },
+    ];
 
 function Slider(){
     return(
         <div className="album py-5">
-                <div className="album-container container">
+                <div className="album-container">
 
                 <div className="album-row">
                     <div className="album-title">
-                        <h1>Popular</h1>
+                        <h2>Popular</h2>
                     </div>
                     <div className="album-cards">
-                        <div className="g-card">
-                            <div className="image-container">
-                                <a href="#">
-                                <img className="card-pic"/>
-                                </a>
+                        {featuredMovies.map((movie) =>{
+                            return (
+                            <div className="g-card">
+                                <div className="image-container">
+                                    <a href="#">
+                                    <img className="card-pic" src={movie.image}/>
+                                    </a>
+                                </div>
+                                <div className="card-title-area">
+                                <h4 className="card-title">{movie.title}</h4>
+                                </div>
                             </div>
-                            <div className="card-title-area">
-                                <h3 className="card-title">Tenet</h3>
-                            </div>
-                        </div>
-                        <div className="g-card">
-                            <div className="image-container">
-                                <a href="#">
-                                <img className="card-pic"/>
-                                </a>
-                            </div>
-                            <div className="card-title-area">
-                                <h3 className="card-title">Tenet</h3>
-                            </div>
-                        </div>
-                        <div className="g-card">
-                            <div className="image-container">
-                                <a href="#">
-                                <img className="card-pic"/>
-                                </a>
-                            </div>
-                            <div className="card-title-area">
-                                <h3 className="card-title">Tenet</h3>
-                            </div>
-                        </div>
-                        <div className="g-card">
-                            <div className="image-container">
-                                <a href="#">
-                                <img className="card-pic"/>
-                                </a>
-                            </div>
-                            <div className="card-title-area">
-                                <h3 className="card-title">Tenet</h3>
-                            </div>
-                        </div>
-                        <div className="g-card">
-                            <div className="image-container">
-                                <a href="#">
-                                <img className="card-pic"/>
-                                </a>
-                            </div>
-                            <div className="card-title-area">
-                                <h3 className="card-title">Tenet</h3>
-                            </div>
-                        </div>
+                            )
+                        })}
+                        
                     </div>
-                    
 
                     </div>
 
