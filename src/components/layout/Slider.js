ef8de8aee43beaedc5c726a372904a2e5fccf77f
gDoe24 <gregkeel11@gmail.com>
2021-02-08 12:49:59 -0500

    feat: Home Slider section Added Slider.js to control the sliders for popular, action, genres, etc. Implemented design for rows and cards within the slider

---
+++ src/components/layout/Slider.js
@@ -0,0 +1,74 @@
+import React, { Fragment } from 'react';
+
+
+function Slider(){
+    return(
+        <div className="album py-5">
+                <div className="album-container container">
+
+                <div className="album-row">
+                    <div className="album-title">
+                        <h1>Popular</h1>
+                    </div>
+                    <div className="album-cards">
+                        <div className="g-card">
+                            <div className="image-container">
+                                <a href="#">
+                                <img className="card-pic"/>
+                                </a>
+                            </div>
+                            <div className="card-title-area">
+                                <h3 className="card-title">Tenet</h3>
+                            </div>
+                        </div>
+                        <div className="g-card">
+                            <div className="image-container">
+                                <a href="#">
+                                <img className="card-pic"/>
+                                </a>
+                            </div>
+                            <div className="card-title-area">
+                                <h3 className="card-title">Tenet</h3>
+                            </div>
+                        </div>
+                        <div className="g-card">
+                            <div className="image-container">
+                                <a href="#">
+                                <img className="card-pic"/>
+                                </a>
+                            </div>
+                            <div className="card-title-area">
+                                <h3 className="card-title">Tenet</h3>
+                            </div>
+                        </div>
+                        <div className="g-card">
+                            <div className="image-container">
+                                <a href="#">
+                                <img className="card-pic"/>
+                                </a>
+                            </div>
+                            <div className="card-title-area">
+                                <h3 className="card-title">Tenet</h3>
+                            </div>
+                        </div>
+                        <div className="g-card">
+                            <div className="image-container">
+                                <a href="#">
+                                <img className="card-pic"/>
+                                </a>
+                            </div>
+                            <div className="card-title-area">
+                                <h3 className="card-title">Tenet</h3>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                    </div>
+
+                </div>
+                </div>
+    )
+}
+
+export default Slider;
